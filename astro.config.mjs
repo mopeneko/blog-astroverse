4a4c3aa0c5e80547df87cabf2f8cc756602d1705
mopeneko <mopeneko@lem0n.cc>
2024-07-24 03:13:12 +0900

    Add priority, changefreq to sitemap loc

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,55 @@ export default defineConfig({
   },
   integrations: [
     mdx(),
-    sitemap(),
+    sitemap({
+      serialize(item) {
+        const { pathname } = new URL(item.url);
+
+        // Top
+        if (pathname === "/") {
+          item.priority = 1.0;
+          item.changefreq = "daily";
+          return item;
+        }
+
+        // Tags
+        if (pathname === "/tags/") {
+          item.priority = 0.5;
+          item.changefreq = "monthly"
+          return item;
+        }
+
+        // Page
+        if (/^\/page\/[\d]+\/$/.test(pathname)) {
+          item.priority = 0.6;
+          item.changefreq = "weekly";
+          return item;
+        }
+
+        // Article
+        if (/^\/posts\/[\w-]+\/$/.test(pathname)) {
+          item.priority = 0.8;
+          item.changefreq = "weekly";
+          return item;
+        }
+
+        // Tag
+        if (/^\/tags\/[\w%]+\/[\d]+\/$/.test(pathname)) {
+          item.priority = 0.5;
+          item.changefreq = "monthly";
+          return item;
+        }
+
+        // Category
+        if (/^\/category\/[\w%]+\/[\d]+\/$/.test(pathname)) {
+          item.priority = 0.5;
+          item.changefreq = "monthly";
+          return item;
+        }
+
+        return item;
+      }
+    }),
     pagefind(),
     tailwind(),
     partytown({
